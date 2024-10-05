3a24d948ad674bb14716c1e567c313029a37abb4
BCHansenCL <143536935+BCHansenCL@users.noreply.github.com>
2024-10-05 18:30:40 -0400

    Changes python!

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for # type: ignore
 import sqlite3
+
+def add_event(loc,t,s):
+    cursor.execute('INSERT INTO rides(location,timestamp,seats) VALUES(?,?,?)',(location,time,seats))
+
+
 conn = sqlite3.connect('eaglerides.db')
 cursor = conn.cursor()
 cursor.execute('''
@@ -10,10 +15,10 @@ cursor.execute('''
         seats INTEGER
     )
 ''')
-location = "harvard"
+location = "test"
 time = 1728324000
 seats = 3
-cursor.execute('INSERT INTO rides(location,timestamp,seats) VALUES(?,?,?)',(location,time,seats))
+add_event(location,time,seats)
 conn.commit()
 conn.close()
 app = Flask(__name__)
