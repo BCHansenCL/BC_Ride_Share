9a6b66fdb876362c3b00f1a7de5a82c987516ec6
TitoKolop <titoisawesome77@gmail.com>
2024-10-06 11:46:05 -0400

    goodbye email

--- app.py
+++ app.py
@@ -54,40 +54,6 @@ def submit():
         conn.commit()  # Commit the changes
     return redirect(url_for('index'))
 
-@app.route('/email')
-def email():
-    return render_template(email.html)
-
-@app.route('/send', methods = ['POST'])
-def send():
-    eml = request.form.get("email")
-    sender_email = "eagleridebc@gmail.com"
-    receiver_email = eml
-    password = "hthrtac1524"
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
-    msg['Subject'] = "Eagle Ride Code"
-    code = random.random()
-    body = "Your code is" + code
-    msg.attach(MIMEText(body, 'plain'))
-    try:
-        # Setup the SMTP server
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        
-        server.login(sender_email, password)
-
-        text = msg.as_string()
-        server.sendmail(sender_email, receiver_email, text)
-        
-        print("Email sent successfully!")
-        
-    except Exception as e:
-        print(f"Failed to send email: {e}")
-    finally:
-        server.quit()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
