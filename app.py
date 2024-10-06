0b9cb209735558b205704be81e4aa13e9515fbaf
BCHansenCL <143536935+BCHansenCL@users.noreply.github.com>
2024-10-05 20:40:38 -0400

    Did some stuff

--- app.py
+++ app.py
@@ -26,16 +26,25 @@ app = Flask(__name__)
 # In-memory list to store posts
 posts = []
 
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
-@app.route('/post', methods=['POST'])
+
+@app.route('/post')
 def post():
-    content = request.form['content']
-    if content:
-        posts.append(content)
-    return redirect(url_for('index'))
+    return render_template('post.html')
+
+# @app.route('/post', methods=['POST'])
+# def post():
+#    content = request.form['content']
+#    if content:
+#        posts.append(content)
+#    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
